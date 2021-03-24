f03e2f6c5bc8b2e7bae899a5ad2bbcf7d28f6231
alexboagreek <olivesargreek@gmail.com>
2021-03-24 08:54:29 +0300

    modified: scripot.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -4,7 +4,7 @@ module.exports = function countCats(matrix) {
   let catCounter = 0;
      matrix.forEach(function(item){
        for (let i = 0; i < item.length; i++){
-         item[i] === "^^" && catCounter ++;
+         item[i] == "^^" && catCounter ++;
        }
      })
  
